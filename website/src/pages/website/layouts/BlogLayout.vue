c3acd2ac82e1ffee2e8e6f9fb66059c4b9d6ab76
maria <mariahendrikx@gmail.com>
2024-06-23 16:49:50 +0200

    added marked library

--- website/src/pages/website/layouts/BlogLayout.vue
+++ website/src/pages/website/layouts/BlogLayout.vue
@@ -1,40 +1,33 @@
 <template>
-  <div>
-    <div v-if="isLoading" class="loading">Loading...</div>
-    <div v-else v-html="readmeContent"></div>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      isLoading: false,
-      readmeContent: ''
-    };
-  },
-  mounted() {
-    this.fetchReadme();
-  },
-  methods: {
-    async fetchReadme() {
-      try {
-        this.isLoading = true;
-        const response = await fetch('ReadMe-blogs/extreme_programming_20240609.md');
-        this.readmeContent = await response.text();
-      } catch (error) {
-        console.error('Error fetching README:', error);
-      } finally {
-        this.isLoading = false;
-      }
-    }
+  <!-- {{ markdown }} -->
+    <textarea v-model="markdown"></textarea>
+    <div v-html="markdownToHtml"></div>
+  
+  </template>
+  <script>
+  import {marked} from 'marked';
+  export default {
+    name: 'App',
+   data(){
+     return {
+       markdown:  "# Hello World",
+     };
+   },
+   computed: {
+     markdownToHtml(){
+      // return "## Hello World"
+        return marked(this.markdown);
+     }
+   }
   }
-};
-</script>
-
-<style>
-.loading {
-  font-size: 16px;
-  padding: 20px;
-}
-</style>
+  </script>
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
